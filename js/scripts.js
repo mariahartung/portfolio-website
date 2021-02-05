6797766232fc5c7af5ac43004cd70ae58d28c541
mariahartung <76693011+mariahartung@users.noreply.github.com>
2021-02-05 21:21:25 +0100

    Add javascript code for mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log("Hello and welcome to my portfolio site, where I share some of my latest UX work. Enjoy your visit! - Maria Hartung, UX designer based near cologne.");
+
+function menuToggle() {
+  var mQuery = window.matchMedia('(max-width: 640px)');
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle' && mQuery.matches) {
+    x.className += 'responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
