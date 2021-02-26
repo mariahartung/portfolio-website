9a2e3b38effa938d25d829de3480e459d152997c
mariahartung <76693011+mariahartung@users.noreply.github.com>
2021-02-26 17:08:07 +0100

    Fix errors and warnings

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-console.log("Hello and welcome to my portfolio site, where I share some of my latest UX work. Enjoy your visit! - Maria Hartung, UX designer based near cologne.");
+console.log('Hello and welcome to my portfolio site, where I share some of my latest UX work. Enjoy your visit! - Maria Hartung, UX designer based near cologne.')
 
-function menuToggle() {
-  var mQuery = window.matchMedia('(max-width: 640px)');
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var mQuery = window.matchMedia('(max-width: 640px)')
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle' && mQuery.matches) {
-    x.className += 'responsive';
+    x.className += 'responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
