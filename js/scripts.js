fc6f83502c29aefb56fe950325aca4832fdc8ac0
mariahartung <76693011+mariahartung@users.noreply.github.com>
2022-04-10 11:54:19 +0200

    Optimize mobile images

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-console.log('Hello and welcome to my portfolio site, where I share some of my latest UX work. Enjoy your visit! - Maria Hartung, UX designer based near cologne.')
+console.log('Hello and welcome to my portfolio site where I share some of my latest UX work. Enjoy your visit! - Maria Hartung, UX designer based near cologne.')
 
 function menuToggle () {
   var mQuery = window.matchMedia('(max-width: 640px)')
